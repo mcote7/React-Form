92e84a1a4e8b15f02adcb7995f7298403a2bbc29
Michael Cote <michaelcote7@outlook.com>
2020-05-08 00:07:03 -0700

    first react form need fix message display on submit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import UserForm from './components/UserForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="bgimage"></div>
+        <UserForm />
+        <br/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/UserForm.js
@@ -0,0 +1,73 @@
+import React, { useState } from  'react';
+// import {render} from 'react-dom';
+
+
+
+const UserForm = (props) => {
+    const [firstname, setFirstname] = useState("");
+    const [lastname, setLastname] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmpass, setConfirmpass] = useState("");
+      
+    const userString = `${firstname} ${lastname}`;
+
+    const helloUser = (event) => {
+        return (
+            <div className="row">
+            <div className="formcontainer">
+                <div className="inputcontainer">
+                    <div className="outputcontainer">&bull; Hello, {firstname} welcome to react! &bull;</div>
+                </div>
+            </div>
+        </div>
+        );
+    };
+    const createUser = (e) => {
+        e.preventDefault();
+        const newUser = { firstname, lastname, email, password, confirmpass };
+        console.log("Welcome", newUser);
+    };
+    return(
+    <>
+        <form onSubmit={ createUser }>
+            <div className="formcontainer">
+                <div className="inputcontainer">
+                    <label>First name &bull; &nbsp; </label>
+                    <input type="text" onChange={ (e) => setFirstname(e.target.value) } />
+                </div>
+                <div className="inputcontainer">
+                    <label>Last name &bull; &nbsp; </label>
+                    <input type="text" onChange={ (e) => setLastname(e.target.value) } />
+                </div>
+                <div className="inputcontainer">
+                    <label>Email Address &bull; &nbsp; </label>
+                    <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+                </div>
+                <div className="inputcontainer">
+                    <label>Password &bull; &nbsp; </label>
+                    <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+                </div>
+                <div className="inputcontainer">
+                    <label>Confirm Password &bull; &nbsp; </label>
+                    <input type="password" onChange={ (e) => setConfirmpass(e.target.value) } />
+                </div>
+                <div className="row">
+                    <button type="submit" value="createUser" className="btncote" onClick={ (event) => helloUser()}>SUBMIT</button>
+                </div>
+            </div>
+        </form>
+        <br/>
+        <br/>
+        <div className="row">
+            <div className="formcontainer">
+                <div className="inputcontainer">
+                    <div className="outputcontainer">&bull; User info &bull; <br/> {userString} <br/> {email} </div>
+                </div>
+            </div>
+        </div>
+        </>
+        );
+    };
+
+export default UserForm;
